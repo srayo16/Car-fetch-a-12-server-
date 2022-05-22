bd29eca088d158d3348db2534be8f74b7e9a6a5b
srayo16 <srayosikder@gmail.com>
2022-05-22 15:32:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Alhamdilillah, server is running');
+})
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
