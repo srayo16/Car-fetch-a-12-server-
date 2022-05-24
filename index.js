ffbc5d9f416aa13d52deef11ecde29a5add39ef1
srayo16 <srayosikder@gmail.com>
2022-05-24 23:43:53 +0600

    payment added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
+const stripe = require('stripe')(process.env.REVEL_TEST_API_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -11,13 +12,15 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dysjk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log('DB Connected');
+
 
 async function run() {
     try {
         await client.connect();
+        console.log('DB Connected');
         const partsDatabase = client.db("PartsSupplier").collection("Parts");
         const bookingsDatabase = client.db("PartsSupplier").collection("bookings");
+        const paymentDatabase = client.db("PartsSupplier").collection("payments");
 
         app.get('/parts', async (req, res) => {
             const query = {};
@@ -33,6 +36,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/booking', async (req, res) => {
+            const query = {};
+            const result = await bookingsDatabase.find(query).toArray();
+            res.send(result);
+
+        })
+
         app.post('/booking', async (req, res) => {
             const booked = req.body;
             const result = await bookingsDatabase.insertOne(booked);
@@ -46,8 +56,60 @@ async function run() {
 
             res.send(result);
         })
+        //start
+
+        app.get('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const booking = await bookingsDatabase.findOne(query);
+            res.send(booking);
+        })
+        app.patch('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const paymentDetails = req.body;
+            // console.log(paymentDatabase);
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: paymentDetails.transactionId
+                }
+            };
+
+            const result = await bookingsDatabase.updateOne(filter, updateDoc);
+            const paymentSend = await paymentDatabase.insertOne(updateDoc);
+            // sendPaymentConfirmationEmail(paymentDetails);
+
+            res.send(result);
 
 
+        })
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const bookings = req.body;
+            const amount = bookings.price;
+            // console.log(amount);
+
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ['card']
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+
+        //end
+        app.delete('/booking', async (req, res) => {
+            const id = req.query.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingsDatabase.deleteOne(query);
+            res.send(result);
+        })
 
 
     } finally {
