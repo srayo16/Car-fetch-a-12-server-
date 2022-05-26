46359cffc5bf20d6f173099e074de6844bef8bf2
srayo16 <srayosikder@gmail.com>
2022-05-26 20:38:31 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
@@ -9,9 +10,23 @@ const stripe = require('stripe')(process.env.REVEL_TEST_API_KEY);
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dysjk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 
 async function run() {
@@ -25,7 +40,8 @@ async function run() {
         const informationDatabase = client.db("PartsSupplier").collection("userInformation");
         const usersDatabase = client.db("PartsSupplier").collection("users");
 
-        app.get('/parts', async (req, res) => {
+
+        app.get('/parts', verifyJWT, async (req, res) => {
             const query = {};
             const result = await partsDatabase.find(query).toArray();
             res.send(result);
@@ -55,7 +71,7 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/booking', async (req, res) => {
+        app.get('/booking', verifyJWT, async (req, res) => {
             const query = {};
             const result = await bookingsDatabase.find(query).toArray();
             res.send(result);
@@ -83,7 +99,7 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/review', async (req, res) => {
+        app.get('/review', verifyJWT, async (req, res) => {
             const query = {};
             const result = await reviewDatabase.find(query).toArray();
             res.send(result);
@@ -116,7 +132,7 @@ async function run() {
 
         })
 
-        app.get('/user', async (req, res) => {
+        app.get('/user', verifyJWT, async (req, res) => {
             const users = await usersDatabase.find().toArray();
             res.send(users);
         });
@@ -159,7 +175,8 @@ async function run() {
                 $set: user,
             };
             const result = await usersDatabase.updateOne(filter, updateDoc, options);
-            res.send(result);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
         });
 
         //start
